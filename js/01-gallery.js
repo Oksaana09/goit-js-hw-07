411dde2063af40df866e01d658a24f474899d245
Oksaana09 <126173861+Oksaana09@users.noreply.github.com>
2023-07-31 12:51:52 +0300

    js7 3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -34,15 +34,14 @@ gallery.addEventListener('click', e => {
 
     const selectedImage = e.target.getAttribute('data-source')
 
-    const instance = basicLightbox.create(`
-        < img src="${selectedImage}" width="800" height="600">`,{
-            onShow: (instance) => {
-                window.addEventListener('keydown', onEscKeyPress);
-            },
-            onClose: (instance) => {
-                window.removeEventListener('keydown', onEscKeyPress);
-            },   
-    })      
+    const instance = basicLightbox.create(`html, {
+        onShow: (instance) =>
+        console.log(onShow, instance),
+        onClose: (instance) =>
+        console.log(onClose, instance)
+    }
+   /*  <img src="${selectedImage}" width="800" height="600"> */
+`)
 
 
     instance.show()
